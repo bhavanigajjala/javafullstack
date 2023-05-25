720c34d7438f291ec5724121fb1874fc0465f5d8
bhavanigajjala <132564336+bhavanigajjala@users.noreply.github.com>
2023-05-25 10:49:32 +0530

    Add files via upload

---
+++ MainLog.java
@@ -0,0 +1,12 @@
+
+    public class MainLog {
+
+        public static void main(String[] args) {
+
+            IDandPasswords idandPasswords = new IDandPasswords();
+
+            LoginPage loginPage = new LoginPage(idandPasswords.getLoginInfo());
+
+        }
+    }
+
